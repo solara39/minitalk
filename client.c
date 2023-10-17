52f0b616c33f426b7af2951ec04e47d4dd4a1056
sola <119765665+solara39@users.noreply.github.com>
2023-10-17 18:42:14 +0900

    Add files via upload

---
+++ client.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skondo <skondo@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/14 17:25:54 by skondo            #+#    #+#             */
+/*   Updated: 2023/10/14 18:07:01 by skondo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <string.h>
+#include <signal.h>
+#include <stdlib.h>
+
+void	send_chr(const pid_t pid, char c)
+{
+	int	bits;
+
+	bits = 7;
+	while (bits >= 0)
+	{
+		if (c & (1 << bits))
+			kill(pid, SIGUSER1);
+		else
+			kill(pid, SIGUSER2);
+		bits--;
+	}
+}
+
+void	send_str(const pid_t pid, char *str)
+{
+	while (*str)
+	{
+		send_chr(pid, *str);
+		str++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int	pid;
+
+	if (argc != 3)
+		return (1);
+	pid = atoi(argv[1]);//要改善
+	send_str(pid, argv[2]);
+}
--- server.c
+++ server.c
@@ -1,11 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skondo <skondo@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/14 17:26:00 by skondo            #+#    #+#             */
+/*   Updated: 2023/10/15 19:03:54 by skondo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+
+volatile sig_atomic_t	bits = 0;
+
+void	signal_handler(int signum, siginfo_t *info, void *dummy)
+{
+	static int	i;
+	char		c;
+
+	bits = bits << 1;
+	if (signum == SIGUSER1)
+		bits |= 1;
+	else if (signum == SIGUSER2)
+		;
+	i++;
+	c = "0xff" & bits;
+	if (i == 8)
+	{
+		write(1, &c, 1);
+		i = 0;
+	}
+}
+
 int	main()
 {
-	struct sigaction sa;
-	if (-1 == sigemptyset(&sa.sa_mask)
+	struct sigaction	sa1;
+	struct sigaction	sa2;
+
+	if (-1 == sigemptyset(&sa1.sa_mask)
 		exit(1);
-	sa.sa_handler = signal_handler;
-	sa.sa_flags = 0;
-	if (-1 == sigaction(SIGINT, &sa, NULL)
+	if (-1 == sigemptyset(&sa2.sa_mask)
 		exit(1);
+	sa1.sa_handler = signal_handler;
+	sa2.sa-handler = signal_handler;
+	sa1.sa_flags = SA_SIGINFO;
+	sa2.sa_flags = SA_SIGINFO;
+	if (-1 == sigaction(SIGINT, &sa1, NULL)
+		exit(1);
+	if (-1 == sigaction(SIGINT, &sa2, NULL)
+		exit(1);
+	ft_printf("%d",getpid());
 	return (0);
 }
